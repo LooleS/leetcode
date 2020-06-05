9bfba0037dca4cc7e798fbb9a74c3ed91dbfe8be
LooleS <2865134664@qq.com>
2020-06-05 09:52:30 +0800

    Looles

---
+++ 4.寻找两个有序数组的中位数.java
@@ -0,0 +1,93 @@
+/*
+ * @lc app=leetcode.cn id=4 lang=java
+ *
+ * [4] 寻找两个有序数组的中位数
+ *
+ * https://leetcode-cn.com/problems/median-of-two-sorted-arrays/description/
+ *
+ * algorithms
+ * Hard (36.85%)
+ * Likes:    2721
+ * Dislikes: 0
+ * Total Accepted:    205.1K
+ * Total Submissions: 536.5K
+ * Testcase Example:  '[1,3]\n[2]'
+ *
+ * 给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。
+ * 
+ * 请你找出这两个正序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ * 
+ * 你可以假设 nums1 和 nums2 不会同时为空。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ * 
+ * 则中位数是 2.0
+ * 
+ * 
+ * 示例 2:
+ * 
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ * 
+ * 则中位数是 (2 + 3)/2 = 2.5
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        int m = nums1.length;
+        int n = nums2.length;
+        if (m > n) { // 实现 m<=n
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+            int tmp = m;
+            m = n;
+            n = tmp;
+        }
+        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+            if (i < iMax && nums2[j - 1] > nums1[i]) {
+                iMin = i + 1; // i 太小，需要增大
+            } else if (i > iMin && nums1[i - 1] > nums2[j]) {
+                iMax = i - 1; // i 太大，需要减小
+            } else { // i 为合适值，根据不同情况返回不同值
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = nums2[j - 1];
+                } else if (j == 0) {
+                    maxLeft = nums1[i - 1];
+                } else {
+                    maxLeft = Math.max(nums1[i - 1], nums2[j - 1]);
+                }
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+
+                int minRight = 0;
+                if (i == m) {
+                    minRight = nums2[j];
+                } else if (j == n) {
+                    minRight = nums1[i];
+                } else {
+                    minRight = Math.min(nums2[j], nums1[i]);
+                }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+    }
+}
+// @lc code=end
+
